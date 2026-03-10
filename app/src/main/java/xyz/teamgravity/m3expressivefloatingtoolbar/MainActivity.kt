91fa4a1713162a1d97cc78931656347a3a3cb729
raheem <raheemideadev@gmail.com>
2026-03-10 22:26:00 +0500

    Setup Material3;

--- app/src/main/java/xyz/teamgravity/m3expressivefloatingtoolbar/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/m3expressivefloatingtoolbar/MainActivity.kt
@@ -5,12 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import xyz.teamgravity.m3expressivefloatingtoolbar.ui.theme.M3ExpressiveFloatingToolbarTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +15,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             M3ExpressiveFloatingToolbarTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { padding ->
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    M3ExpressiveFloatingToolbarTheme {
-        Greeting("Android")
     }
 }
